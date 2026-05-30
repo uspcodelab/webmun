228ca6d3c7dac39ac071db620645b529c7f906c9
Le-frances13 <100540487+Le-frances13@users.noreply.github.com>
2026-05-30 14:27:28 -0300

    Login Page, Homepage Beggings

--- frontend/src/pages/Login_page.tsx
+++ frontend/src/pages/Login_page.tsx
@@ -1,28 +1,26 @@
 
 import { GalleryVerticalEnd } from "lucide-react"
-import { LoginForm } from "@/components/login-form"
+import { LoginForm } from "@/components/auth/login-form"
 export default function LoginPage() {
   return (
     <div className="grid min-h-svh lg:grid-cols-2">
-      <div className="flex flex-col gap-4 p-6 md:p-10">
+      <div className="flex flex-col gap-4 p-6 md:p-2">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="#" className="flex items-center gap-2 font-medium">
-            <div className="flex size-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-            Acme Inc.
+            <img src="Images\branding\logo.png" alt="WebMun logo" className="md:h-24 sm:h-12 w-auto object-contain" />
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
           <div className="w-full max-w-xs">
+        
             <LoginForm />
           </div>
         </div>
       </div>
       <div className="relative hidden bg-muted lg:block">
         <img
-          src="/placeholder.svg"
-          alt="Image"
+          src="\Images\auth\LoginSideImage.png"
+          alt="LoginSideImage"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
         />
       </div>
