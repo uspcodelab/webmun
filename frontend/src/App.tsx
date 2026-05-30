69e8a4aef2aeb6694fd7e955af0e410034a424e2
Le-frances13 <100540487+Le-frances13@users.noreply.github.com>
2026-05-30 16:19:07 -0300

    Create Comitte Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SessionPage from './pages/Session';
 import LoginPage from './pages/Login_page';
+import Home from './pages/Home';
+import CreateCommittee from './pages/CreateComittee';
 
 function App() {
 	// We'll use react-router in order to develop our SPA 
 	return (
 		<BrowserRouter>
 			<Routes>
+				<Route path="/" element={<Home />} />
 				<Route path="/commitees/:commiteeId/session" element={<SessionPage />} />
 				<Route path="/login" element={<LoginPage />} />
+				<Route path="/create-committee" element={<CreateCommittee />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div className="flex h-screen w-screen flex-col">
+            <h1>Home Page</h1>
+        </div>
+    )
+}
