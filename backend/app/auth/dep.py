503524208112821a96fb471b08cb02917613f59e
wate <110754234+r0liveir@users.noreply.github.com>
2026-08-01 02:17:29 -0300

    fix: fix remaining issues with ci

--- backend/app/auth/dep.py
+++ backend/app/auth/dep.py
@@ -29,13 +29,13 @@ def get_current_user(
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             # TODO: map this out to an outer table, since both WS and HTTP uses this
-            detail="token_expired",  
+            detail="token_expired",
             headers={"WWW-Authenticate": "Bearer"},
         ) from e
     except TokenInvalidError as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             # TODO: map this out to an outer table, since both WS and HTTP uses this
-            detail="token_invalid",  
+            detail="token_invalid",
             headers={"WWW-Authenticate": "Bearer"},
         ) from e
