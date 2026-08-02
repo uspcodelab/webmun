dad204033dc3685cb5a2989dc2e85fbe89d885df
Le-frances13 <100540487+Le-frances13@users.noreply.github.com>
2026-08-02 11:58:21 +0200

    fix: app routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,11 +10,12 @@ import DashHome from './components/userDashboard/dash-home.tsx';
 import CreateCommittee from './pages/CreateCommittee';
 import SessionPage from './pages/Session';
 import BasicInfo from './components/userDashboard/conferenceMenus/about/MUN-basic-info';
+
 import ParticipantListAllocation from './components/userDashboard/conferenceMenus/participants/list-allocation';
 import TeamAllocate from './components/userDashboard/conferenceMenus/team/team-alocate';
 import CommitteeManagement from './components/userDashboard/conferenceMenus/committee-managemant';
-
-
+import ConferenceDocs from './components/userDashboard/conferenceMenus/about/conference-docs';
+import ManageTeamsRoles from './components/userDashboard/conferenceMenus/team/manage-teams-roles';
 
 function App() {
 	// We'll use react-router in order to develop our SPA 
@@ -29,9 +30,11 @@ function App() {
 				<Route path="/signup" element={<SignupPage />} />
 				<Route path="/dashboard" element={<UserDash />}>
 					<Route index element={<DashHome />} />
-					<Route path="committees" element={<CommitteeManagement />} />
 					<Route path="conference/about/basic-info" element={<BasicInfo />} />
-					<Route path="conference/team/teams-management" element={<TeamAllocate />} />
+					<Route path="conference/about/docs" element={<ConferenceDocs />} />
+					<Route path="committees" element={<CommitteeManagement />} />
+					<Route path="conference/team/onboarding" element={<TeamAllocate />} />
+					<Route path="conference/team/teams-management" element={<ManageTeamsRoles />} />
 					<Route path="conference/participants/list-allocation" element={<ParticipantListAllocation />} />
 				</Route>
 				<Route path="/create-committee" element={<CreateCommittee />} />
