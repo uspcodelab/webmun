ec9beee59e1efc5e054b60375776f30347221bcf
Extroias <91105220+Extroias@users.noreply.github.com>
2026-08-07 00:15:40 -0300

    feat:translation on some enums to facilitate frontend use

--- backend/app/session/enums.py
+++ backend/app/session/enums.py
@@ -61,33 +61,33 @@ class ChairEvents(StrEnum):
 
 # --- Additional Info ---
 class DebateTypes(StrEnum):
-    SPEAKERS_LIST = "Speakers List"
-    MODERATED_DEBATE = "Moderated Debate"  # During this type, the queue to speak should not be automatic
-    UNMODERATED_DEBATE = "Unmoderated Debate"
+    SPEAKERS_LIST = "Lista de Discursos"
+    MODERATED_DEBATE = "Debate Moderado"  # During this type, the queue to speak should not be automatic
+    UNMODERATED_DEBATE = "Debate não Moderado"
 
 
 class Motions(StrEnum):
     CHANGE_DEBATE_TYPE = "Mudar Tipo de Debate"
-    POSTPONE_SESSION = "Adiaamento de Sessão"
-    REOPEN_SESSION = "Reabrir Sessão"
+    POSTPONE_SESSION = "Adiamento de Sessão"
+    REOPEN_SESSION = "Reabertura de Sessão"
     TOUR_DE_TABLE = "Tour de Table"
     END_DEBATE = "Encerramento de Debate"  # TODO: map this out since "motion to close debate" means clear GSL and go to voting procedures in modeldiplomat and can also mean the same as "motion to move into voting procedures"
     VOTE_AMENDMENT = "Votação de Emenda"  # TODO: check the way this is used, since amendments MUST be voted if they're present during VOTING_PROCEDURES
     VOTE_BY_ROLL_CALL = "Votação por Chamada"  # TODO: check the way this is used
     CLOSE_SPEAKERS_LIST = "Fechamento da Lista de Discursos"
-    REOPEN_SPEAKERS_LIST = "Reabrir a Lista de Discursos"
-    SPLIT_PROPOSAL = "Divisão de Proposta"
-    INTRODUCE_RESOLUTION_PROPOSAL = "Introdução de Proposta de Resolução"
-    INTRODUCE_AMENDMENT_PROPOSAL = "Introdução de Proposta de Emenda"
+    REOPEN_SPEAKERS_LIST = "Reabertura de Lista de Discursos"
+    SPLIT_PROPOSAL = "Divisão da Proposta"
+    INTRODUCE_RESOLUTION_PROPOSAL = "Introdução da Proposta de Resolução"
+    INTRODUCE_AMENDMENT_PROPOSAL = "Introdução da Proposta de Emenda"
     CHANGE_TOPIC = "Mudança de Tópico"
-    QUORUM = "Quórum"
+    QUORUM = "Contagem de Quórum"
     CUSTOM_MOTION = ""  # not implemented
 
 
 class Questions(StrEnum):
-    ORDER = "Order"
-    QUESTION = "Question"
-    PERSONAL_PRIVILEGE = "Personal Privilege"
+    ORDER = "Ordem"
+    QUESTION = "Questão"
+    PERSONAL_PRIVILEGE = "Privilégio Pessoal"
 
 
 class RollCallChoice(StrEnum):
