8150864fe53d3e6a668860a11b3685da567f0502
Extroias <91105220+Extroias@users.noreply.github.com>
2026-08-31 22:14:10 -0300

    fix: fix typo

--- backend/app/session/engine.py
+++ backend/app/session/engine.py
@@ -1014,7 +1014,7 @@ def handle_end_speech(
             message="Cannot end speech while timer is running"
         ) 
 
-    if state.current_state == States.MODERATED_CAUCUS and state.debate.speech_count:
+    if state.current_state == States.MODERATED_CAUCUS and state.debate.total_speeches:
         state.debate.speech_count -= 1
 
     state.previous_speakers.append(state.current_speaker)
